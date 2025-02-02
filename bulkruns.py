4e87cf4ced3ecb645056f5bb7c411664f2e4f9dc
fatemehramezani <fatemehramezani>
2025-02-02 13:44:53 +0100

    organize

--- bulkruns.py
+++ bulkruns.py
@@ -10,13 +10,15 @@ with open(csv_file, newline='', encoding='utf-8') as file:
     values = [row[0] for row in reader]  # Assuming values are in the first column
 
 # Base command for yt-dlp
-base_command = 'yt-dlp -P "./MyVideos" -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"'
+#base_command = 'yt-dlp -P "./MyVideos" -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"'
+#base_command = 'yt-dlp -P "~/E://Training/Youtube/MyVideos" -o "%(playlist_index)s - %(title)s.%(ext)s"'
+base_command = 'yt-dlp -P "~/E://Training/Youtube/MyVideos" -o "%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"'
 base_command = base_command + ' -f "(bv*+ba/b)[protocol^=http][protocol!*=dash] / (bv*+ba/b)"'
 base_command = base_command + ' --split-chapters'
 base_command = base_command + ' --ffmpeg-location "./ffmpeg"'
 #base_command = base_command + ' --cookies-from-browser chrome --cookies "./cookies.txt"'
-base_command = base_command + '--cookies-from-browser Edge  --cookies "./cookies.txt"'
-# base_command = base_command + ' --default-search "ytsearch"'
+base_command = base_command + ' --cookies "./cookies.txt"'
+base_command = base_command + ' --default-search "ytsearch"'
 # base_command = base_command + '--proxy "socks5://127.0.0.1:7890"'
 base_command = base_command + ' --sleep-interval 200'
 
